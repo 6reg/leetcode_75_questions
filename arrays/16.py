8b355e60688f07a2b08c3667f689cd28fa1f2b2c
greg mathias <mathiasgreg@gmail>
2021-12-07 20:06:49 -0500

    16

---
+++ arrays/16.py
@@ -0,0 +1,8 @@
+def twoSum(nums, target):
+    hm = {}
+    for i, v in enumerate(nums):
+        diff = target - nums[i]
+        if diff in hm:
+            return [hm[diff], i]
+        hm[v] = i
+print(twoSum([1,3,4], 7))
