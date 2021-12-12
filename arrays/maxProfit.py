afabccb7219f27df6c6e23ed9d06dd7d780668ff
6reg <mathiasgreg@gmail.com>
2021-12-11 20:52:12 -0500

    maxProfit

---
+++ arrays/maxProfit.py
@@ -0,0 +1,9 @@
+def maxProfit(prices):
+    max_profit, min_price = 0, float('inf')
+    for price in prices:
+        min_price = min(min_price, price)
+        profit = price - min_price
+        max_profit = max(max_profit, profit)
+    return max_profit
+
+print(maxProfit([1, 3, 5, 8]))
