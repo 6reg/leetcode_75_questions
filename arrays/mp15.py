8fc66dd370f95056ff935b602edcf84e319f586b
6reg <mathiasgreg@gmail.com>
2022-01-06 19:42:19 -0500

    mp14

---
+++ arrays/mp15.py
@@ -0,0 +1,9 @@
+def mp(prices):
+    max_profit, min_price = 0, float('inf')
+    for price in prices:
+        min_price = min(min_price, price)
+        profit = price - min_price
+        max_profit = max(max_profit, profit)
+    return max_profit
+
+print(mp([1,3,5,3,44,5]))
