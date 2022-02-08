d824afcda4766d873bd8ed45e6eb8525243641bf
6reg <mathiasgreg@gmail.com>
2022-02-07 20:08:58 -0500

    hmmn'

---
+++ hmmm.rb
@@ -0,0 +1,11 @@
+def two_sum nums, target
+  hm = Hash.new
+  nums.each_with_index do |num, index|
+    diff = hm[target - num]
+    return [diff, index] if diff != nil
+  hm[num] = index
+
+  end
+end
+
+puts two_sum([2,3,4,5,6], 11)
